794976079cf197d36c4f713628a62b85be5b319f
Anders Pearson <anders@columbia.edu>
2011-10-30 16:49:25 -0400

    model changed to handle gearphoto

--- gear/models.py
+++ gear/models.py
@@ -25,7 +25,6 @@ class Gear(models.Model):
     description = models.TextField(default="",blank=True)
     tags = tagging.fields.TagField()
     links = generic.GenericRelation(Link)
-    photos = generic.GenericRelation(Photo)
     added = models.DateTimeField(auto_now_add=True,editable=False)
     modified = models.DateTimeField(auto_now=True,editable=False)
 
@@ -42,17 +41,13 @@ class Gear(models.Model):
         LinkFormset = generic.generic_inlineformset_factory(Link, extra=1)
         return LinkFormset(instance=self)
 
-    def photos_formset(self):
-        PhotoFormset = generic.generic_inlineformset_factory(Photo, extra=1)
-        return PhotoFormset(instance=self)
-
     def save(self):
         self.slug = slugify(self.manufacturer.name + "-" + self.name)[:256]
         super(Gear, self).save()
 
     def first_photo(self):
-        if self.photos.count() > 0:
-            return self.photos.all()[0]
+        if self.gearphoto_set.count() > 0:
+            return self.gearphoto_set.all()[0].photo
         else:
             return None
 
@@ -65,13 +60,6 @@ class Gear(models.Model):
                 model = Link
                 exclude = ('content_object','content_type','object_id')
         return LinkForm
-
-    def add_photo_form(self):
-        class PhotoForm(ModelForm):
-            class Meta:
-                model = Photo
-                exclude = ('content_object','content_type','object_id')
-        return PhotoForm
 
 class GearPhoto(models.Model):
     gear = models.ForeignKey(Gear)
