8337c10f12324609686f0df3970e031b8447d36a
Anders Pearson <anders@columbia.edu>
2011-10-30 17:49:28 -0400

    clearing out photo model

---
+++ photo/migrations/0002_auto__del_field_photo_object_id__del_field_photo_content_type.py
@@ -0,0 +1,40 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Deleting field 'Photo.object_id'
+        db.delete_column('photo_photo', 'object_id')
+
+        # Deleting field 'Photo.content_type'
+        db.delete_column('photo_photo', 'content_type_id')
+
+
+    def backwards(self, orm):
+        
+        # User chose to not deal with backwards NULL issues for 'Photo.object_id'
+        raise RuntimeError("Cannot reverse this migration. 'Photo.object_id' and its values cannot be restored.")
+
+        # User chose to not deal with backwards NULL issues for 'Photo.content_type'
+        raise RuntimeError("Cannot reverse this migration. 'Photo.content_type' and its values cannot be restored.")
+
+
+    models = {
+        'photo.photo': {
+            'Meta': {'object_name': 'Photo'},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'caption': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('sorl.thumbnail.fields.ImageWithThumbnailsField', [], {'max_length': '100'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'source_name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '256', 'blank': 'True'}),
+            'source_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['photo']
--- photo/models.py
+++ photo/models.py
@@ -28,28 +28,11 @@ class Photo(models.Model):
     source_name = models.CharField(max_length=256,blank=True,default="")
     source_url = models.URLField(blank=True)
 
-    content_type = models.ForeignKey(ContentType)
-    object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
-
     added = models.DateTimeField(auto_now_add=True,editable=False)
     modified = models.DateTimeField(auto_now=True,editable=False)
 
-    def is_gear(self):
-        return self.content_object.model == "gear"
-
-    def is_musiciangear(self):
-        return self.content_object.model == "musiciangear"
-
-    def is_musician(self):
-        return self.content_object.model == "musician"
-
-    def gear(self):
-        return self.content_object
-
     def label(self):
         return unicode(self.content_object)
-
 
     def get_absolute_url(self):
         return "/photos/%d/" % self.id
@@ -57,12 +40,8 @@ class Photo(models.Model):
     def type_display(self):
         return "Photo"
 
-    def name(self):
-        return "Photo of " + self.content_object.name
-
-
 class PhotoInline(generic.GenericTabularInline):
     model = Photo
 
-PhotoFormset = generic.generic_inlineformset_factory(Photo, extra=1)
+#PhotoFormset = generic.generic_inlineformset_factory(Photo, extra=1)
 
