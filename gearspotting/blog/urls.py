8b5c9266eebced7194fbfd16775e80b9e7576320
Anders Pearson <anders@columbia.edu>
2013-04-20 14:06:30 -0400

    individual blog post page. plus some basic schema.org

--- gearspotting/blog/urls.py
+++ gearspotting/blog/urls.py
@@ -4,5 +4,8 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'gearspotting.blog.views',
     url(r'^$', 'index'),
-    url(r'^post/$', 'post'),
+    url(r'^post/$', 'add_post'),
+    url((r'(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/'
+         r'(?P<username>\w+)/(?P<slug>[\w\-]+)/$'),
+        'post'),
 )
--- gearspotting/blog/views.py
+++ gearspotting/blog/views.py
@@ -1,4 +1,6 @@
 from .models import Post
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
 from django.template.defaultfilters import slugify
 
@@ -11,7 +13,7 @@ def index(request):
     )
 
 
-def post(request):
+def add_post(request):
     if request.method == 'POST':
         if not request.POST.get('body', False):
             return redirect("/blog/post/")
@@ -28,4 +30,18 @@ def post(request):
         request,
         "blog/add_post.html",
         {},
+    )
+
+
+def post(request, year, month, day, username, slug):
+    author = get_object_or_404(User, username=username)
+    post = get_object_or_404(
+        Post,
+        author=author,
+        slug=slug,
+    )
+    return render(
+        request,
+        "blog/post.html",
+        {'post': post},
     )
