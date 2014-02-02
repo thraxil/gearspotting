3b2c179905dac161b56fff7c3b0a70c1cb7ba768
Anders Pearson <anders@columbia.edu>
2014-02-02 15:36:05 -0500

    remove unused rendered_with

--- gearspotting/main/views.py
+++ gearspotting/main/views.py
@@ -1,25 +1,7 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response
 from django.views.generic.base import TemplateView
 from gearspotting.gear.models import Gear
 from gearspotting.musician.models import Musician
 from gearspotting.blog.models import Post
-
-
-class rendered_with(object):
-    def __init__(self, template_name):
-        self.template_name = template_name
-
-    def __call__(self, func):
-        def rendered_func(request, *args, **kwargs):
-            items = func(request, *args, **kwargs)
-            if isinstance(items, dict):
-                return render_to_response(
-                    self.template_name, items,
-                    context_instance=RequestContext(request))
-            else:
-                return items
-        return rendered_func
 
 
 class IndexView(TemplateView):
