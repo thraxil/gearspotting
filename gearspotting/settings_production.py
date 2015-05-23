ebd0ea726ff44b68b16aa777a971036813ac8781
Anders Pearson <anders@columbia.edu>
2015-05-23 16:52:50 +0200

    s3 + cloudfront

--- gearspotting/settings_production.py
+++ gearspotting/settings_production.py
@@ -1,8 +1,9 @@
 # flake8: noqa
 from settings_shared import *
+import os
 
 TEMPLATE_DIRS = (
-    "/var/www/gearspotting/gearspotting/gearspotting/templates",
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 MEDIA_ROOT = '/var/www/gearspotting/uploads/'
@@ -18,7 +19,23 @@ TEMPLATE_DEBUG = DEBUG
 STATICFILES_DIRS = ()
 STATIC_ROOT = "/var/www/gearspotting/gearspotting/media/"
 
+AWS_S3_CUSTOM_DOMAIN = "d2l0eqxka0mdmr.cloudfront.net"
+AWS_IS_GZIPPED = True
+
+AWS_STORAGE_BUCKET_NAME = "thraxil-gearspotting-static-prod"
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
+S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
+COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+DEFAULT_FILE_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
+MEDIA_URL = S3_URL + '/media/'
+COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
+AWS_QUERYSTRING_AUTH = False
 
 if 'migrate' not in sys.argv:
     INSTALLED_APPS = INSTALLED_APPS + [
