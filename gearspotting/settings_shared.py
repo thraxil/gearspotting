d85082a0ff4abca94fb090475486142d2d4f0a1d
Anders Pearson <anders@thraxil.org>
2018-03-24 17:10:43 +0000

    thraxilsettings

--- gearspotting/settings_shared.py
+++ gearspotting/settings_shared.py
@@ -1,89 +1,15 @@
 # Django settings for gearspotting project.
 import os.path
-import sys
+from thraxilsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+base = os.path.dirname(__file__)
+locals().update(common(app='gearspotting', base=base))
 
-ADMINS = ()
+# extended common settings
 
-MANAGERS = ADMINS
+ALLOWED_HOSTS += ['.gearspotting.ccom', '127.0.0.1']  # noqa
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gearspotting',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-    }
-}
-
-if 'test' in sys.argv or 'harvest' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '', }}
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/gearspotting/uploads/"
-MEDIA_URL = '/uploads/'
-ADMIN_MEDIA_PREFIX = '/media/'
-STATIC_URL = "/media/"
-SECRET_KEY = 'WHUHGASD%TYJMSAFDS'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [
-            "/var/www/gearspotting/gearspotting/templates/",
-            os.path.join(os.path.dirname(__file__), "templates"),
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-)
-
-ROOT_URLCONF = 'gearspotting.urls'
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
+INSTALLED_APPS += [  # noqa
     'taggit',
     'gearspotting.main',
     'gearspotting.musician',
@@ -96,61 +22,7 @@ INSTALLED_APPS = [
     'gearspotting.profile',
     'django.contrib.sitemaps',
     'gearspotting.blog',
-    'django_statsd',
-    'gunicorn',
-    'django_markwhat',
-    'compressor',
-    'smoketest',
-    'raven.contrib.django.raven_compat',
 ]
 
-ACCOUNT_ACTIVATION_DAYS = 7
-
-LOGIN_REDIRECT_URL = '/accounts/%(username)s/'
-LOGIN_URL = '/accounts/signin/'
-LOGOUT_URL = '/accounts/signout/'
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[gearspotting] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "gearspotting@ccnmtl.columbia.edu"
-
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', 'sitemedia'),
-)
-
-STATIC_URL = "/media/"
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../media/")),
-)
-STATIC_ROOT = ""
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',  # default
-)
-ANONYMOUS_USER_ID = -1
-AUTH_PROFILE_MODULE = 'profile.MyProfile'
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'gearspotting'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-STATSD_PATCHES = ['django_statsd.patches.db', ]
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
-
-ALLOWED_HOSTS = ['localhost', '.gearspotting.com']
 RETICULUM_BASE = "http://reticulum.thraxil.org/"
 RETICULUM_PUBLIC_BASE = "https://d2f33fmhbh7cs9.cloudfront.net/"
